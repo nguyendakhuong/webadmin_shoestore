2f7cb99eeb2d31d17fdcb256ce6e712e4acd4014
Cuongtoz <tocuong20102003@gmail.com>
2024-03-07 17:03:39 +0700

    update cuongto

--- src/modules/components/modal/modalProduct/modalProduct.jsx
+++ src/modules/components/modal/modalProduct/modalProduct.jsx
@@ -7,13 +7,12 @@ import moment from 'moment';
 const ModalProduct = ({ product, onClose, isOpen }) => {
     const [data, setData] = useState(null);
     const [comment, setComment] = useState(null);
+
     useEffect(() => {
         if (isOpen) {
             getProductInfo();
-
         }
     }, [isOpen]);
-
 
     const getProductInfo = async () => {
         const token = APP_LOCAL.getTokenStorage();
@@ -27,7 +26,7 @@ const ModalProduct = ({ product, onClose, isOpen }) => {
             const data = await response.json();
             if (data.status === 200) {
                 setData(data.data);
-                setComment(data.comment)
+                setComment(data.comment);
             } else {
                 onClose();
                 ToastApp.warning(data.message);
@@ -36,34 +35,48 @@ const ModalProduct = ({ product, onClose, isOpen }) => {
             ToastApp.error("Lỗi: " + e);
         }
     };
+
     return (
         isOpen && (
             <div className="dialog-overlay">
-                {data ? (
-                    <div className="dialog">
-                        <h2>Thông tin chi tiết sản phẩm</h2>
-                        <p><strong>Tên:</strong> {data.name}</p>
-                        <p><strong>Giá:</strong> {data.price}</p>
-                        <p><strong>Số lượng:</strong> {data.quantity}</p>
-                        <p><strong>Mô tả:</strong> {data.description}</p>
-                        <p><strong>Giới thiệu:</strong> {data.introduce}</p>
-                        <p><strong>Giá giảm giá:</strong> {data.priceSale ? data.priceSale : "Null"}</p>
-                        <p><strong>Thời gian bắt đầu giảm giá:</strong> {data.timeSaleStart ? moment(data.timeSaleStart).format('DD/MM/YYYY') : "Null"}</p>
-                        <p><strong>Thời gian kết thúc giảm giá:</strong> {data.timeSaleEnd ? moment(data.timeSaleEnd).format('DD/MM/YYYY') : "Null"}</p>
-                        <p><strong>Loại:</strong> {data.category}</p>
-                        <p><strong>Hình ảnh:</strong> </p>
-                        <img src={data.imageProduct} alt='' style={{ width: '200px', height: '200px' }} />
-                        <p><strong>Comment:</strong></p>
-                        {comment.sort((a, b) => new Date(a.createAt) - new Date(b.createAt)).map(commentItem => (
-                            <p key={commentItem.createAt} className="comment-item">
-                                <strong>Email:</strong> {commentItem.email} <br />
-                                <strong>Name:</strong> {commentItem.username} <br />
-                                <strong>Content:</strong> {commentItem.content}
-                            </p>
-                        ))}
-                        <button className="close-button" onClick={onClose}>Đóng</button>
-                    </div>
-                ) : "Đang tải dữ liệu"}
+                <div className="dialog">
+                    <h2>Thông tin chi tiết sản phẩm</h2>
+                    {data ? (
+                        <>
+                            <div className="dialog-content">
+
+                                <div className="image-container">
+                                    <img src={data.imageProduct} alt='' className="product-image" />
+                                </div>
+                                <div className="info-container">
+
+                                    <p><strong>Tên:</strong> {data.name}</p>
+                                    <p><strong>Giá:</strong> {data.price}</p>
+                                    <p><strong>Số lượng:</strong> {data.quantity}</p>
+                                    <p><strong>Mô tả:</strong> {data.description}</p>
+                                    <p><strong>Giới thiệu:</strong> {data.introduce}</p>
+                                    <p><strong>Giá giảm giá:</strong> {data.priceSale ? data.priceSale : "Null"}</p>
+                                    <p><strong>Thời gian bắt đầu giảm giá:</strong> {data.timeSaleStart ? moment(data.timeSaleStart).format('DD/MM/YYYY') : "Null"}</p>
+                                    <p><strong>Thời gian kết thúc giảm giá:</strong> {data.timeSaleEnd ? moment(data.timeSaleEnd).format('DD/MM/YYYY') : "Null"}</p>
+                                    <p><strong>Loại:</strong> {data.category}</p>
+                                </div>
+                            </div>
+                            <div className="comments">
+                                <h2>Comment</h2>
+                                <div className="comment-list">
+                                    {comment.map(commentItem => (
+                                        <div key={commentItem.createAt} className="comment-item">
+                                            <p><strong>Email:</strong> {commentItem.email}</p>
+                                            <p><strong>Name:</strong> {commentItem.username}</p>
+                                            <p><strong>Content:</strong> {commentItem.content}</p>
+                                        </div>
+                                    ))}
+                                </div>
+                            </div>
+                        </>
+                    ) : "Đang tải dữ liệu"}
+                </div>
+                <button className="close-button" onClick={onClose}>Đóng</button>
             </div>
         )
     );
