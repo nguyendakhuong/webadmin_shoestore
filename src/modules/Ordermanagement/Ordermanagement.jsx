38fc9d51138706da8bd48150be1f295de9e25f35
Khương Nguyễn Đa <dakhuong281202@gmail.com>
2024-03-14 16:01:17 +0700

    khuong update

--- src/modules/Ordermanagement/Ordermanagement.jsx
+++ src/modules/Ordermanagement/Ordermanagement.jsx
@@ -1,54 +1,85 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect, useContext } from 'react';
 import './ordermanagenment.scss';
 import APP_LOCAL from '../../lib/localStorage';
 import ToastApp from '../../lib/notification/Toast';
 import OrderDetail from '../components/modal/modalOder/modalOder'
-
+import UserContext from '../../context/use.context';
+import { KEY_CONTEXT_USER } from '../../context/use.reducer';
 
 const OrderManagenment = (order) => {
-    const [confirmedOrderId, setConfirmedOrderId] = useState(null);
+    const [userCtx, dispatch] = useContext(UserContext)
     const [reloadData, setReloadData] = useState(false);
     const [data, setData] = useState(null);
     const [selectedOrder, setSelectedOrder] = useState(null);
 
-    const handleConfirmOrder = () => {
+    const statusLabels = {
+        createOrder: "Đang chờ xác nhận",
+        delivering: "Đang giao hàng",
+        configOrder: "Đã nhận hàng",
+        cancelOrder: "Đang chờ hủy đơn hàng",
+    };
+    const handleConfirmOrder = async (id, e) => {
         const token = APP_LOCAL.getTokenStorage();
-        if (!token) {
-            ToastApp.error('Không có token.');
-            return;
-        }
+        e.stopPropagation();
 
-        const requestOptions = {
-            headers: {
-                Authorization: `Bearer ${token}`,
-            },
-        };
-
-        fetch(`http://localhost:3001/order/configOrder/${order.id}`, requestOptions)
-            .then(res => {
-                return res.json();
-            })
-            .then(data => {
-                if (data.status === 200) {
-                    setData(data.data);
-                } else {
-                    ToastApp.error('Lỗi: ' + data.message);
+        try {
+            const response = await fetch(`http://localhost:3001/order/verifyOrder/${id}`, {
+                method: "GET",
+                headers: {
+                    'Authorization': `Bearer ${token}`
                 }
-            })
-            .catch(e => {
-                console.log(e);
-                ToastApp.error('Lỗi khi gửi yêu cầu.');
             });
+            const data = await response.json();
+            if (data.status === 200) {
+                ToastApp.success("Xác nhận đơn hàng thành công")
+                setReloadData(true)
+            } else {
+                ToastApp.warning("Cảnh báo: " + data.message)
+            }
+
+        } catch (e) {
+            return ToastApp.error("Lỗi hệ thống: " + e)
+        }
+    };
+    const handleCancelOrder = async (id, e) => {
+        e.stopPropagation();
+
+        dispatch({
+            type: KEY_CONTEXT_USER.SHOW_MODAL,
+            payload: {
+                typeModal: 'DELETE_ITEM',
+                dataModal: id,
+                contentModel: "Bạn có chắc chắn muốn hủy đơn hàng " + id + " không?",
+                onClickConfirmModel: async () => {
+                    const token = APP_LOCAL.getTokenStorage()
+                    try {
+                        const response = await fetch(`http://localhost:3001/order/verifyCancelOrder/${id}`, {
+                            method: "GET",
+                            headers: {
+                                'Authorization': `Bearer ${token}`
+                            }
+                        });
+                        const data = await response.json();
+                        if (data.status === 200) {
+                            ToastApp.success("Hủy đơn hàng thành công")
+                            setReloadData(true)
+                        } else {
+                            ToastApp.warning("Cảnh báo: " + data.message)
+                        }
+
+                    } catch (e) {
+                        return ToastApp.error("Lỗi hệ thống: " + e)
+                    }
+                }
+            }
+        })
+
     };
 
-
-    const handleCancelOrder = (id) => {
-        setConfirmedOrderId(null);
-    };
-
-    const handleRowClick = (order) => {
-        setSelectedOrder(order.id);
-    };
+    // const handleRowClick = (order) => {
+    //     setSelectedOrder(order.id);
+    // };
+    console.log(data)
 
     const getOrder = async () => {
         const token = APP_LOCAL.getTokenStorage();
@@ -107,26 +138,27 @@ const OrderManagenment = (order) => {
                                     <th>Địa chỉ</th>
                                     <th>Trạng thái</th>
                                     <th>Hành động</th>
+                                    <th>Hủy đơn hàng</th>
                                 </tr>
                             </thead>
                             <tbody>
                                 {data ? data.map((order, index) => (
-                                    <tr key={order.id} onClick={() => handleRowClick(order)}>
+                                    <tr key={order.id}>
                                         <td>{order.id}</td>
                                         <td>{order.userId}</td>
                                         <td>{order.total}</td>
                                         <td>{order.phone}</td>
                                         <td>{order.address}</td>
-                                        <td>{order.status}</td>
+                                        <td>{statusLabels[order.status]}</td>
                                         <td>
-                                            {confirmedOrderId === order.id ? (
-                                                <>Đã xác nhận</>
+                                            {order.status === "createOrder" ? (
+                                                <button onClick={(e) => handleConfirmOrder(order.id, e)}>Xác nhận</button>
                                             ) : (
-                                                <>
-                                                    <button onClick={() => handleConfirmOrder(order.id)}>Xác nhận</button>
-                                                    <button onClick={() => handleCancelOrder(order.id)}>Hủy</button>
-                                                </>
+                                                <> Đã Xác nhận</>
                                             )}
+                                        </td>
+                                        <td>
+                                            <button onClick={(e) => handleCancelOrder(order.id, e)}>Xác nhận hủy</button>
                                         </td>
                                     </tr>
                                 )) : "Đang tải dữ liệu"}
@@ -134,8 +166,9 @@ const OrderManagenment = (order) => {
                         </table>
                     </div>
                 </div>
-            )}
-        </div>
+            )
+            }
+        </div >
     );
 };
 
